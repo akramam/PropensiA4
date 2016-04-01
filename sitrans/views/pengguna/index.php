8c6fb3081a2f1a7707001154f29b05afdb9a03e3
bintinrc <bintinurcahya@gmail.com>
2016-04-01 15:34:17 +0700

    Insert Yii sitrans

---
+++ sitrans/views/pengguna/index.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Penggunas';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="pengguna-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Pengguna', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'username',
+            'nama',
+            'password',
+            'role',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+</div>

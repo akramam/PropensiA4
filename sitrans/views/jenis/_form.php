f5bc1628cb6b9dbd4f389d1235de7eb4bac96fa1
bintinrc <bintinurcahya@gmail.com>
2016-04-02 17:07:42 +0700

    CRU Jenis

---
+++ sitrans/views/jenis/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Jenis */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="jenis-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'namajenis')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'rop')->textInput() ?>
+
+    <?= $form->field($model, 'stok_kilo')->textInput() ?>
+
+    <?= $form->field($model, 'stok_karton')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ sitrans/views/jenis/update.php
@@ -0,0 +1,23 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Jenis */
+
+$this->title = Yii::t('app', 'Update {modelClass}: ', [
+    'modelClass' => 'Jenis',
+]) . ' ' . $model->idjenis;
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Jenis'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->idjenis, 'url' => ['view', 'id' => $model->idjenis]];
+$this->params['breadcrumbs'][] = Yii::t('app', 'Update');
+?>
+<div class="jenis-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
